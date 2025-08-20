7c4b90c919ad9ada48a7b3e82e1e5e02efd2aec4
Belay <pbelay@gmail.com>
2025-08-20 13:47:35 +0200

    temp

---
+++ public/posts/2025-03-16-LGA-xor1/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Directorio onde están as imaxes
+input_directory="./"
+
+# Ruta do logotipo
+#logo="/home/belay/Documentos/arcoteo/assets/img/about_transparent.png"
+logo="/home/belay/Descargas/logo.png"
+
+# Tamaño do logotipo (axusta segundo as túas necesidades)
+logo_width=200
+logo_height=200
+
+# Recorrer todas as imaxes no directorio
+for image in "$input_directory"/*.{jpg,jpeg,png}; do
+    # Verifica se o arquivo existe
+    if [ -f "$image" ]; then
+        output_image="${image%.*}_logo.${image##*.}"
+
+        # Engadir o logotipo á imaxe
+        convert "$image" "$logo" -gravity southeast -geometry "${logo_width}x${logo_height}+10+10" -composite "$output_image"
+
+        echo "Logotipo engadido con éxito: $output_image"
+    fi
+done
