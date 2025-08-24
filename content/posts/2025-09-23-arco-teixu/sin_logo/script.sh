6ddbbf0cc7091c0256bb43034a530fe4e58b9660
Belay <pbelay@gmail.com>
2025-08-24 12:36:24 +0100

    arco teixu

--- content/posts/2025-09-23-arco-teixu/sin_logo/script.sh
+++ content/posts/2025-09-23-arco-teixu/sin_logo/script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Directorio onde están as imaxes
+# Directorio onde están as imaxes orixinais
 input_directory="./"
 
+# Directorio de saída para as imaxes con logo
+output_directory="./imaxenes-logo"
+
+# Crear o directorio de saída se non existe
+mkdir -p "$output_directory"
+
 # Ruta do logotipo
-#logo="/home/belay/Documentos/arcoteo/assets/img/about_transparent.png"
 logo="logo.png"
 
-# Tamaño do logotipo (axusta segundo as túas necesidades)
+# Tamaño do logotipo
 logo_width=300
 logo_height=300
 
@@ -15,7 +20,8 @@ logo_height=300
 for image in "$input_directory"/*.{jpg,jpeg,png}; do
     # Verifica se o arquivo existe
     if [ -f "$image" ]; then
-        output_image="${image%.*}_logo.${image##*.}"
+        filename=$(basename "$image")
+        output_image="$output_directory/${filename%.*}_logo.${filename##*.}"
 
         # Engadir o logotipo á imaxe
         convert "$image" "$logo" -gravity southeast -geometry "${logo_width}x${logo_height}+10+10" -composite "$output_image"
